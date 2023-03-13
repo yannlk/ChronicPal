40745ba6d72c879ceb04ba92759994f88bbb6d44
y0lky <yannkull@gmail.com>
2023-03-13 22:11:33 +0100

    V 1.2.1.1 Refix Previous Problems with solution

--- tk_commands.py
+++ tk_commands.py
@@ -48,13 +48,17 @@ def process_collector(collector_index, root, collectors, input_date):
 
         def handle_entry(event):
             input_value = entry_data.get()
-            if isinstance(input_value, float) and 0 <= float(input_value) <= 5:
-                handle_submit(collector, input_value, root, collectors, collector_index, input_date)
-            else:
+            try:
+                if 0 <= float(input_value) <= 5:
+                    handle_submit(collector, input_value, root, collectors, collector_index, input_date)
+                else:
+                    raise ValueError
+            except ValueError:
                 error_label = tk.Label(root, text="Please enter a number between 0 and 5.")
                 error_label.pack()
                 ui_entry.delete(0, tk.END)
                 ui_entry.focus_set()
+
 
         ui_entry.bind("<Return>", handle_entry)
 
